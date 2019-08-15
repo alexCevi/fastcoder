aadb1e894c8406a980600bc89add72fb938273c5
gitTwisted <alexcwebstudio@gmail.com>
2019-08-15 19:40:57 -0400

    base files for build

--- fastcoderapp/karma.conf.js
+++ fastcoderapp/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/fastCoderApp'),
+      dir: require('path').join(__dirname, './coverage/fastcoderapp'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
--- fastcoderapp/src/app/app.module.ts
+++ fastcoderapp/src/app/app.module.ts
@@ -1,21 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { InputareaComponent } from './inputarea/inputarea.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    InputareaComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
