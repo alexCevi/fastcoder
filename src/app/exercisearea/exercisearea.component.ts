642d7a02070646a7f183917b2db5627a5ca7827c
gitTwisted <alexcwebstudio@gmail.com>
2019-08-19 19:10:01 -0400

    finished Single page app ui

--- src/app/exercisearea/exercisearea.component.ts
+++ src/app/exercisearea/exercisearea.component.ts
@@ -8,20 +8,20 @@ import { ExerciseDataProviderService } from '../services/exercise-data-provider.
 })
 export class ExerciseareaComponent implements OnInit {
 
-  // topTitle: string;
-  // exerciseData: string[];
-
   topTitle: string;
-  exerciseData: string;
+  exerciseData: string[];
+
+  // topTitle: string;
+  // exerciseData: string;
 
   constructor(public setExerciseType: ExerciseDataProviderService) { }
 
   ngOnInit() {
-    // this.topTitle = this.setExerciseType.exerciseTitle;
-    // this.exerciseData = this.setExerciseType.exerciseData;
+    this.topTitle = this.setExerciseType.exerciseTitle;
+    this.exerciseData = this.setExerciseType.exerciseData;
 
-    this.topTitle = "JavaScript";
-    this.exerciseData = "DATA ONE";
+    // this.topTitle = "JavaScript";
+    // this.exerciseData = "DATA ONE";
   }
 
 }
