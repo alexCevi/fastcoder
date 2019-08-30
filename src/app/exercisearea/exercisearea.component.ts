b5c21f77fdadfcb486e346895de84df2cca27269
gitTwisted <alexcwebstudio@gmail.com>
2019-08-30 07:39:31 -0400

    cleaned up methods and variables in SPA component

--- src/app/exercisearea/exercisearea.component.ts
+++ src/app/exercisearea/exercisearea.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { ExerciseDataProviderService } from '../services/exercise-data-provider.service';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-exercisearea',
@@ -10,25 +9,22 @@ import { Router } from '@angular/router';
 export class ExerciseareaComponent {
 
   topTitle: string;
-  exerciseDataDisplay: string;
-  userDataInput: string;
   clearData: string;
   hasUserStarted = false;
   exerciseData = [];
   currentDisplayedExercise: string;
   currentQuestion = 0;
 
-  constructor(public exerciseService: ExerciseDataProviderService, private router: Router) { }
+  constructor(public exerciseService: ExerciseDataProviderService) { }
 
   validateInput(event: any) {
-    if (this.exerciseDataDisplay === event.target.value) {
+    if (this.currentDisplayedExercise === event.target.value) {
       this.updateExerciseData();
-      this.exerciseDataDisplay = this.currentDisplayedExercise;
       this.clearData = '';
     } else {
       // ADD THIS ON BACK SPACE KEY AND COUNT BACKSPACE KEY
-      // TODO ADD SHAKE ANIMATION FOR WRONG ANSWER
-      // TODO ADD RED GLOW ON CARD
+      // ADD SHAKE ANIMATION FOR WRONG ANSWER
+      // ADD RED GLOW ON CARD
     }
   }
 
@@ -37,8 +33,6 @@ export class ExerciseareaComponent {
     this.topTitle = this.exerciseService.exerciseTitle;
     this.exerciseData = this.exerciseService.exerciseData;
     this.updateExerciseData();
-    this.exerciseDataDisplay = this.currentDisplayedExercise;
-
   }
 
   updateExerciseData() {
--- src/app/services/exercise-data-provider.service.ts
+++ src/app/services/exercise-data-provider.service.ts
@@ -8,21 +8,10 @@ export class ExerciseDataProviderService {
 
   exerciseTitle: string;
   exerciseData: string[];
-  currentDisplayedExercise: string;
-  currentQuestion = 0;
-  exerciseHasCompleted = false;
 
   setExerciseData(exerviseTitle: string, exerciseData: string[]) {
     this.exerciseTitle = exerviseTitle;
     this.exerciseData = exerciseData;
   }
 
-  updateExerciseData() {
-    if (this.currentQuestion < this.exerciseData.length) {
-      this.currentDisplayedExercise = this.exerciseData[this.currentQuestion];
-      this.currentQuestion++;
-    } else {
-      this.exerciseHasCompleted = true;
-    }
-  }
 }
