4b6c7e87292e0fd9f2e186bc5da27e293ac2d4c8
gitTwisted <alexcwebstudio@gmail.com>
2019-08-30 17:54:02 -0400

    added in final typing report WIP

--- src/app/exercisearea/exercisearea.component.ts
+++ src/app/exercisearea/exercisearea.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ExerciseDataProviderService } from '../services/exercise-data-provider.service';
+import { TouchSequence } from 'selenium-webdriver';
 
 @Component({
   selector: 'app-exercisearea',
@@ -10,10 +11,16 @@ export class ExerciseareaComponent {
 
   topTitle: string;
   clearData: string;
-  hasUserStarted = false;
-  exerciseData = [];
   currentDisplayedExercise: string;
+  exerciseAccuracy = 0;
+  exerciseWpm: number;
+  numberOfCorrections = 0;
+  expectedExerciseAccuracy = 0;
+  hasUserStarted = false;
+  exerciseHasFinished = false;
+  exerciseData = [];
   currentQuestion = 0;
+
 
   constructor(public exerciseService: ExerciseDataProviderService) { }
 
@@ -22,10 +29,14 @@ export class ExerciseareaComponent {
       this.updateExerciseData();
       this.clearData = '';
     } else if (event.key === 'Backspace') {
-      console.log('working');
-      // ADD THIS ON BACK SPACE KEY AND COUNT BACKSPACE KEY
-      // ADD SHAKE ANIMATION FOR WRONG ANSWER
-      // ADD RED GLOW ON CARD
+      const exerciseDataElement = document.getElementById('questionCardContainer');
+      exerciseDataElement.setAttribute('style', 'box-shadow: 0 8px 16px 0 red');
+
+      this.numberOfCorrections++;
+
+      setTimeout(() => {
+        exerciseDataElement.setAttribute('style', 'box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);');
+      }, 500);
     }
   }
 
@@ -36,12 +47,27 @@ export class ExerciseareaComponent {
     this.updateExerciseData();
   }
 
+  getAccuracy() {
+    if (this.numberOfCorrections === 0) {
+      this.exerciseAccuracy = 100;
+    } else {
+      this.exerciseAccuracy = 10 * (this.expectedExerciseAccuracy - this.numberOfCorrections / this.expectedExerciseAccuracy);
+      console.log(this.numberOfCorrections, this.expectedExerciseAccuracy);
+    }
+  }
+
   updateExerciseData() {
     if (this.currentQuestion < this.exerciseData.length) {
       this.currentDisplayedExercise = this.exerciseData[this.currentQuestion];
       this.currentQuestion++;
+
+      this.expectedExerciseAccuracy += this.currentDisplayedExercise.length;
     } else {
-      console.log('done');
+      const exerciseReportContainer = document.getElementById('exerciseWrapper');
+      exerciseReportContainer.setAttribute('style', 'height: 700px;');
+      this.exerciseHasFinished = true;
+
+      this.getAccuracy();
     }
   }
 
